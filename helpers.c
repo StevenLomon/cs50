de15c654eb98292521f9120f10ebd927e325c519
Steven Lomon Lennartsson <steven.lennartsson@gmail.com>
2024-07-07 08:50:14 +0200

    Filter (less) implemented and done

--- helpers.c
+++ helpers.c
@@ -1,35 +1,24 @@
 #include "helpers.h"
 #include <math.h>
-#include <stdio.h>
 
-#define KERNELSIZE 3
-#define min(a,b) ((a) < (b) ? (a) : (b))
-#define max(a,b) ((a) > (b) ? (a) : (b))
+int minInt(int n, int m);
+int maxInt(int n, int m);
 void swap(BYTE *a, BYTE *b);
 
-int gxKernel[KERNELSIZE][KERNELSIZE] = { {-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1} };
-int gyKernel[KERNELSIZE][KERNELSIZE] = { {-1, -2, -1}, {0, 0, 0}, {1, 2, 1} };
-
-// Convert image to grayscale
-void grayscale(int height, int width, RGBTRIPLE image[height][width])
+int minInt(int n, int m)
 {
-    int numberOfColorChannels = 3;
-    // We loop through every pixel in the image, find the average RBG values, and set that as the new value
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < width; j++)
-        {
-            // Calculate the average RGB value
-            int RGBAvg = round((image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / numberOfColorChannels);
+    if (n <= m)
+        return n;
+    else
+        return m;
+}
 
-            // Set this value as the value for red, green and blue
-            image[i][j].rgbtRed = RGBAvg;
-            image[i][j].rgbtGreen = RGBAvg;
-            image[i][j].rgbtBlue = RGBAvg;
-        }
-    }
-
-    return;
+int maxInt(int n, int m)
+{
+    if (n >= m)
+        return n;
+    else
+        return m;
 }
 
 void swap(BYTE *a, BYTE *b)
@@ -41,34 +30,96 @@ void swap(BYTE *a, BYTE *b)
     *b = tmp;
 }
 
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    // Loop over all pixels
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            // Take average of red, green, and blue
+            int totalRGBvalue = 0;
+            totalRGBvalue += image[i][j].rgbtRed;
+            totalRGBvalue += image[i][j].rgbtGreen;
+            totalRGBvalue += image[i][j].rgbtBlue;
+            int averageRGB = round(totalRGBvalue / 3.0);
+
+            // Update pixel values
+            image[i][j].rgbtRed = averageRGB;
+            image[i][j].rgbtGreen = averageRGB;
+            image[i][j].rgbtBlue = averageRGB;
+        }
+    }
+    return;
+}
+
+// Convert image to sepia
+void sepia(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            // Compute sepia values
+            // The value will be the minimum of the rounded computation and 255, effectively capping
+            // the result at 255
+            int sepiaRed = minInt(round(.393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen +
+                                        .189 * image[i][j].rgbtBlue),
+                                  255);
+            int sepiaGreen =
+                minInt(round(.349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen +
+                             .168 * image[i][j].rgbtBlue),
+                       255);
+            int sepiaBlue = minInt(round(.272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen +
+                                         .131 * image[i][j].rgbtBlue),
+                                   255);
+
+            // Update pixel with sepia values
+            image[i][j].rgbtRed = sepiaRed;
+            image[i][j].rgbtGreen = sepiaGreen;
+            image[i][j].rgbtBlue = sepiaBlue;
+        }
+    }
+    return;
+}
+
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
     // Find the "mirror line"
     int mirrorLine = round(width / 2);
-    // printf("%i\n", mirrorLine);
 
-    // Find the "mirror pixel" for each pixel and swap them
+    // Loop through every row in the image
     for (int i = 0; i < height; i++)
     {
+        // Find the "mirror pixel" for each pixel in the row and swap them
         for (int j = 0; j < mirrorLine; j++)
         {
             // printf("Original pixel: %i, %i\n", i, j);
-            // printf("Mirror pixel: %i, %i\n", i, 600 - j - 1);
+            // printf("Mirror pixel: %i, %i\n", i, mirrorLine - j - 1);
 
             // Swap the RGB values for each pixel pair
-            swap(&image[i][j].rgbtRed, &image[i][600 - j - 1].rgbtRed);
-            swap(&image[i][j].rgbtGreen, &image[i][600 - j - 1].rgbtGreen);
-            swap(&image[i][j].rgbtBlue, &image[i][600 - j - 1].rgbtBlue);
+            swap(&image[i][j].rgbtRed, &image[i][width - j - 1].rgbtRed);
+            swap(&image[i][j].rgbtGreen, &image[i][width - j - 1].rgbtGreen);
+            swap(&image[i][j].rgbtBlue, &image[i][width - j - 1].rgbtBlue);
         }
     }
-
     return;
 }
 
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    // Create a copy of the original image
+    RGBTRIPLE copy[height][width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+            copy[i][j] = image[i][j];
+    }
+
+    // Read pixels’ colors from copy but write pixels’ colors in image
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
@@ -79,75 +130,27 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             int totalBlue = 0;
             int count = 0;
 
-            // max(0, i - 1) ensures that we don't try to access a pixel above the current one if we're in the top row
-            // i - 1 is the pixel above and if we are at the top row (i == 0), we use i = 0 rather than i = -1
-            // min (height - 1, i + 1) ensures that we don't try to access a pixel below the current one if we're in the bottom row
-            // i + 1 is the pixel below and if we are in the bottom row (i == height - 1), we use i = height - 1 rather than i = height
-            // Very similar logic for j and width
-            for (int k = max(0, i - 1); k <= min(height - 1, i + 1); k++)
+            // max(0, i - 1) ensures that we don't try to access a pixel above the current one if
+            // we're in the top row i - 1 is the pixel above and if we are at the top row (i == 0),
+            // we use i = 0 rather than i = -1 min (height - 1, i + 1) ensures that we don't try to
+            // access a pixel below the current one if we're in the bottom row i + 1 is the pixel
+            // below and if we are in the bottom row (i == height - 1), we use i = height - 1 rather
+            // than i = height Very similar logic for j and width
+            for (int k = maxInt(0, i - 1); k <= minInt(height - 1, i + 1); k++)
             {
-                for (int l = max(0, j - 1); l <= min(width - 1, j + 1); l++)
+                for (int l = maxInt(0, j - 1); l <= minInt(width - 1, j + 1); l++)
                 {
-                    totalRed += image[k][l].rgbtRed;
-                    totalGreen += image[k][l].rgbtGreen;
-                    totalBlue += image[k][l].rgbtBlue;
+                    totalRed += copy[k][l].rgbtRed;
+                    totalGreen += copy[k][l].rgbtGreen;
+                    totalBlue += copy[k][l].rgbtBlue;
                     count++;
                 }
             }
 
             // Set the new average "blurred" values as the new values
-            image[i][j].rgbtRed = round(totalRed / count);
-            image[i][j].rgbtGreen = round(totalGreen / count);
-            image[i][j].rgbtBlue = round(totalBlue / count);
-        }
-    }
-    return;
-}
-
-// Detect edges
-void edges(int height, int width, RGBTRIPLE image[height][width])
-{
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < width; j++)
-        {
-            // printf("i: %i, j: %i, Red: %i, Green: %i, Blue: %i\n", i, j, image[i][j].rgbtRed, image[i][j].rgbtGreen, image[i][j].rgbtBlue);
-            int totalGxRed = 0, totalGxGreen = 0, totalGxBlue = 0;
-            int totalGyRed = 0, totalGyGreen = 0, totalGyBlue = 0;
-
-            // In this case we do allow k and l to be negative values, going outside of the boundaries of the image
-            // since we will set the color values to be zero here anyway
-            for (int k = i - 1; k <= i + 1; k++)
-            {
-                for (int l = j - 1; l <= j + 1; l++)
-                {
-                    // It's only if we're actually looking at a pixel inside the boundaries of the image that we grab the RGB values
-                    if (k >= 0 && l >= 0)
-                    {
-                        // Kernel index variables
-                        int m = k + 1, n = l + 1;
-                        // printf("Red: %i, Green: %i, Blue: %i", image[k][l].rgbtRed, image[k][l].rgbtGreen, image[k][l].rgbtBlue);
-                        // There is a discrepancy in how we index the kernels and the the pixel. -1, -1 for the pixel is the topleft pixel but that is 0,0 in the kernel
-                        // Gx
-                        totalGxRed += image[k][l].rgbtRed * gxKernel[m][n];
-                        totalGxGreen += image[k][l].rgbtGreen * gxKernel[m][n];
-                        totalGxBlue += image[k][l].rgbtBlue * gxKernel[m][n];
-
-                        // Gy
-                        totalGyRed += image[k][l].rgbtRed * gyKernel[m][n];
-                        totalGyGreen += image[k][l].rgbtGreen * gyKernel[m][n];
-                        totalGyBlue += image[k][l].rgbtBlue * gyKernel[m][n];
-                    }
-                }
-            }
-
-            // Set the new average "edge detenced" values as the new values (capped at 255)
-            int totalRed = min(255, round(sqrt(totalGxRed) + sqrt(totalGyRed)));
-            int totalGreen = min(255, round(sqrt(totalGxGreen) + sqrt(totalGyGreen)));
-            int totalBlue = min(255, round(sqrt(totalGxBlue) + sqrt(totalGyBlue)));
-            image[i][j].rgbtRed = totalRed;
-            image[i][j].rgbtGreen = totalGreen;
-            image[i][j].rgbtBlue = totalBlue;
+            image[i][j].rgbtRed = round(totalRed / (float) count);
+            image[i][j].rgbtGreen = round(totalGreen / (float) count);
+            image[i][j].rgbtBlue = round(totalBlue / (float) count);
         }
     }
     return;
